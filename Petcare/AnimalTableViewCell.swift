3248e1cd930074e5b1edfb25ed46449d4e6aefe2
feliborgez <feliborgez@gmail.com>
2016-11-29 10:54:42 -0300

    Adding cell

---
+++ Petcare/AnimalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnimalTableViewCell.swift
+//  Petcare
+//
+//  Created by Felipe Borges on 29/11/16.
+//  Copyright © 2016 Felipe Borges. All rights reserved.
+//
+
+import UIKit
+
+class AnimalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
