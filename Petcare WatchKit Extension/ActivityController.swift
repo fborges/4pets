7bef2eda1c99967fac4087a46ff7fe741bf09d61
Gustavo Gomes de Oliveira <gustavogomesdeoliveira@MacBook-Pro-de-Gustavo.local>
2016-12-09 11:36:49 -0300

    add timer to activity controller

--- Petcare WatchKit Extension/ActivityController.swift
+++ Petcare WatchKit Extension/ActivityController.swift
@@ -12,4 +12,38 @@ import WatchKit
 
 class ActivityController: WKInterfaceController{
     
+    
+    @IBOutlet var activityImage: WKInterfaceImage!
+    
+    @IBOutlet var activityLabel: WKInterfaceLabel!
+    
+    @IBOutlet var activityDateLabel: WKInterfaceLabel!
+    
+    @IBOutlet var countdownTimer: WKInterfaceTimer!
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        
+        
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
+        
+        //INSERIR A DATA A PARTIR DA DATA PASSADA PELA ATIVIDADE EM QUESTÃO
+        self.countdownTimer.setDate(NSDate(timeIntervalSinceNow: 10) as Date)
+        self.countdownTimer.start()
+        
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
 }
