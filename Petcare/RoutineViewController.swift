81a9806b30a7d231b5e05c9771905899302288da
Raul Marques de Oliveira <raulmo1337@gmail.com>
2016-12-13 08:21:08 -0300

    working on edit routine

--- Petcare/RoutineViewController.swift
+++ Petcare/RoutineViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class RoutineViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -17,24 +18,40 @@ class RoutineViewController: UIViewController, UITableViewDelegate, UITableViewD
     var pet: Pet?
     var routineType: Int!
     var routineArray = [Routine]()
+    var routine: Routine!
+    var routineIdentifier: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         switch routineType {
         case 0:
+            routineIdentifier = "Bath"
+            routine = pet?.routine!.array[0] as! Routine
             routineArray.append(pet?.routine!.array[0] as! Routine)
         case 1:
+            routineIdentifier = "Hair"
+            routine = pet?.routine!.array[1] as! Routine
             routineArray.append(pet?.routine!.array[1] as! Routine)
         case 2:
+            routineIdentifier = "Claws"
+            routine = pet?.routine!.array[2] as! Routine
             routineArray.append(pet?.routine!.array[2] as! Routine)
         case 3:
+            routineIdentifier = "Teeth"
+            routine = pet?.routine!.array[3] as! Routine
             routineArray.append(pet?.routine!.array[3] as! Routine)
         case 4:
+            routineIdentifier = "Vaccination"
+            routine = pet?.routine!.array[4] as! Routine
             routineArray.append(pet?.routine!.array[4] as! Routine)
         case 5:
+            routineIdentifier = "Deworming"
+            routine = pet?.routine!.array[5] as! Routine
             routineArray.append(pet?.routine!.array[5] as! Routine)
         case 6:
+            routineIdentifier = "Recreation"
+            routine = pet?.routine!.array[6] as! Routine
             routineArray.append(pet?.routine!.array[6] as! Routine)
         default:
             print("error")
@@ -42,26 +59,95 @@ class RoutineViewController: UIViewController, UITableViewDelegate, UITableViewD
         
     }
     
-    @IBAction func add(_ sender: Any) {
-        //        let dao = CoreDataDAO<Recreation>()
-        //        let recreation = dao.new()
-        //
-        //        let dateFormatter = DateFormatter()
-        //        dateFormatter.dateFormat = "yyyy-MM-dd"
-        //
-        //        recreation.date = dateFormatter.date(from: self.dateTf.text!) as NSDate?
-        //        recreation.pet = self.pet
-        //
-        //        // adding to pets array os baths
-        //        let petBaths = pet?.recreation as! NSMutableOrderedSet
-        //        petBaths.add(recreation)
-        //
-        //        dao.insert(recreation)
-        //        recreationArray.append(recreation)
-        //        tableView.reloadData()
+    func scheduleForFequency(hour: Int, minute: Int, amPm: String, frequency: String) -> DateComponents {
+        var dateComponent = DateComponents()
+        var fixedHour: Int!
         
+        if amPm == "PM" {
+            fixedHour = hour + 12
+        } else {
+            fixedHour = hour
+        }
+        
+        switch frequency {
+        case "Daily":
+            dateComponent.hour = fixedHour
+            dateComponent.minute = minute
+        case "3 times a week":
+            dateComponent.day = 3
+            dateComponent.hour = fixedHour
+            dateComponent.minute = minute
+        case "5 times a week":
+            dateComponent.day = 5
+            dateComponent.hour = fixedHour
+            dateComponent.minute = minute
+        case "Weekly":
+            dateComponent.day = 7
+            dateComponent.hour = fixedHour
+            dateComponent.minute = minute
+        case "Monthly":
+            dateComponent.day = 30
+            dateComponent.hour = fixedHour
+            dateComponent.minute = minute
+        case "Yearly":
+            dateComponent.day = 365
+            dateComponent.hour = fixedHour
+            dateComponent.minute = minute
+        default:
+            dateComponent.minute = 0
+        }
+        
+        
+        return dateComponent
     }
     
+    
+    
+    func updateHourAndFrequency() {
+        
+        var hour: Int!
+        var minute: Int!
+        var amPm: String!
+        var frequency: String!
+       
+        let calendar = Calendar.autoupdatingCurrent
+        let notification = UNMutableNotificationContent()
+        
+            notification.body = "Just remind you about \((self.pet?.name)!) \(routine.name)"
+           // notification.badge = NSNumber(value: badgeNumber + 1)
+        
+        // setting routine date
+//            hour = Int(((routineCell.routineHour.title(for: .normal)?.components(separatedBy: ":"))?[0])!)
+//            minute = Int(((routineCell.routineHour.title(for: .normal)?.components(separatedBy: ":"))?[1])!)
+//            amPm = routineCell.routineAmPm.text
+        
+            let dateComponents = scheduleForFequency(hour: hour!, minute: minute!, amPm: amPm! ,frequency: frequency)
+            routine.date = calendar.date(from: dateComponents) as NSDate? //routineTime
+            
+            // setting routine frequency
+            routine.frequency = frequency
+            
+            // updating notification
+        
+
+            UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [routineIdentifier])
+        
+
+        }
+        
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            let request = UNNotificationRequest(identifier: "routine\(index)", content: notification, trigger: trigger)
+            UNUserNotificationCenter.current().add(request, withCompletionHandler:{ (error) in
+                if error != nil {
+                    print(error?.localizedDescription ?? "--")
+                }
+            })
+            
+            
+        
+        
+    }
+
     
     // MARK: - TableView methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
