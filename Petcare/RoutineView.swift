8117a99d8fd57ebbfa1c839330f6d95fa67495dd
feliborgez <feliborgez@gmail.com>
2016-12-13 09:48:16 -0300

    Adding RoutineView

---
+++ Petcare/RoutineView.swift
@@ -0,0 +1,21 @@
+//
+//  RoutineView.swift
+//  Petcare
+//
+//  Created by Felipe Borges on 13/12/16.
+//  Copyright © 2016 Felipe Borges. All rights reserved.
+//
+
+import UIKit
+
+class RoutineView: UIAlertView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
